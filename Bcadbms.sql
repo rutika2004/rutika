174a715e7d2bce5c514a176433a69d5f8494bcaa
rutika2004 <113979203+rutika2004@users.noreply.github.com>
2022-09-20 14:35:53 +0530

    Create Bcadbms.sql

---
+++ Bcadbms.sql
@@ -0,0 +1,19 @@
+create table Student9 ( No int(2), Name varchar(15), School varchar(15), Hobby varchar(20), Std int(1), Gender varchar(1) );
+insert into Student9 (NO , Name , School , Hobby , Std , Gender)
+values (1,'Ajit','Xavier','Dance', 5, 'M'),
+ (2,'Alisha','Sharda','Singing', 5, 'M'),
+ (3,'Pratixa','Aone','Cricket', 5, 'M'),
+ (4,'Deepam','HBK','Painting', 6, 'M'),
+ (5,'Nisha','SantKabir','Cricket', 7, 'M'),
+ (6,'Anya','Xavier','Singing', 6, 'M'),
+ (7,'Raj','Aone','Painting', 6, 'M'),
+ (8,'Monish','Sharda','karate', 6, 'M'),
+ (9,'Ram','Aone','Dance', 6, 'M'),
+ (10,'Shyam','SantKabir','Singing', 6, 'M'),
+ (11,'Kavisha','Xavier','Swimming', 7, 'F'),
+ (12,'Nayan','Aone','Cricket', 7, 'M'),
+ (13,'Meet','Somlalit','Cricket', 7, 'M'),
+ (14,'Urvi','Sharda','Dance', 7, 'F'),
+ (15,'Kunj','Xavir','Singing', 8, 'M');
+ 
+select * from Student9;
